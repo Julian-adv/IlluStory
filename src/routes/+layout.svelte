0e9aefc49aadfcad704d66a0938c0b76242e8c65
Julian <julian.adventerer.dev@gmail.com>
2023-09-05 11:20:34 +0900

    Add popover to each of nav item

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
   import '../app.css'
-  import { Navbar, NavBrand, NavUl, NavHamburger, NavLi } from 'flowbite-svelte'
+  import { Navbar, NavBrand, NavUl, NavHamburger, NavLi, Popover } from 'flowbite-svelte'
   import { page } from '$app/stores'
 
   $: activeUrl = $page.url.pathname
   let activeClass = 'text-sky-400'
+
+  let popovers = [
+    { id: 'cards', text: 'Cards' },
+    { id: 'write', text: 'Write' },
+    { id: 'write_char', text: 'Write character' },
+    { id: 'play', text: 'Play' },
+    { id: 'about', text: 'About' },
+    { id: 'settings', text: 'Settings' }
+  ]
 </script>
 
 <Navbar class="sticky top-0 bg-stone-100 z-30" let:hidden let:toggle color="primary">
@@ -27,7 +36,7 @@
   </NavBrand>
   <NavHamburger on:click={toggle} />
   <NavUl {activeUrl} {hidden} {activeClass} class="p-0">
-    <NavLi href="/">
+    <NavLi id="cards" href="/">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -41,7 +50,7 @@
           d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
       </svg>
     </NavLi>
-    <NavLi href="/write">
+    <NavLi id="write" href="/write">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -55,7 +64,7 @@
           d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
       </svg>
     </NavLi>
-    <NavLi href="/write_char">
+    <NavLi id="write_char" href="/write_char">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -69,7 +78,7 @@
           d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
       </svg>
     </NavLi>
-    <NavLi href="/play">
+    <NavLi id="play" href="/play">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -83,7 +92,7 @@
           d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
       </svg>
     </NavLi>
-    <NavLi href="/about">
+    <NavLi id="about" href="/about">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -97,7 +106,7 @@
           d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
       </svg>
     </NavLi>
-    <NavLi href="/settings">
+    <NavLi id="settings" href="/settings">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -114,5 +123,11 @@
     </NavLi>
   </NavUl>
 </Navbar>
+
+{#each popovers as { id, text }}
+  <Popover class="w-auto h-auto text-sm z-30" triggeredBy={'#' + id}>
+    <span>{text}</span>
+  </Popover>
+{/each}
 
 <slot />
