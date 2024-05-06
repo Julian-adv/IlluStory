1cdc6aa75720083f7479512338b8db6c45c5e1da
Julian <julian.adventerer.dev@gmail.com>
2024-05-06 18:11:45 +0900

    modify transButtonClass

--- src/routes/play/Input.svelte
+++ src/routes/play/Input.svelte
@@ -148,7 +148,9 @@
     currentIndex = $inputHistory.length
   })
 
-  $: transButtonClass = `px-[0.5rem] ${translateButtonClass(state.state === 'translated')}`
+  $: transButtonClass = `self-start px-[0.5rem] ${translateButtonClass(
+    state.state === 'translated'
+  )}`
   $: {
     if (value) {
       state.state = 'translated'
@@ -192,7 +194,7 @@
         d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
     </svg>
   </Button>
-  <Button color="alternative" size="sm" class="mx-2" on:click={send}
+  <Button color="alternative" size="sm" class="self-start mx-2" on:click={send}
     ><svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
